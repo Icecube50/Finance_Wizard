343168c833115c19806fc34e8149215d325b4acd
Mia <icecube5002@gmail.com>
2023-05-14 16:46:28 +0200

    initial template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="text-center mt-5">Trending Movies 🍿</h2>
+        <p>Keep up with the hottest movies that are trending this week.</p>
+      </div>
+
+      <div class="my-4">
+        <a href="#" class="mx-3 h4"> Trending today</a>
+        <a href="#" class="mx-3 h4">This week</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
